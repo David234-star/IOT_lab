dc76f07beab58b3dfc23afc296c19c9b997bbb3d
B Ranadeer <davidbasiripogu@hotmail.com>
2023-07-04 15:46:12 +0530

    done with tasks of IoT

---
+++ led_control/led_control.ino
@@ -0,0 +1,21 @@
+int led = 2;
+int entry = 0;
+void setup() {
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    entry = Serial.read();
+    if(entry == '1'){
+      digitalWrite(led,HIGH);
+    }
+    else if(entry == '0'){
+      digitalWrite(led,LOW);
+    }
+    else{
+      Serial.println("Enter only 0 or 1 control the serial monitor");
+    }
+  }
+}
